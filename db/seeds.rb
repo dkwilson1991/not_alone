fa75bb31f915d6d7819d74dbadc026b73777e6d2
Keita Wilson <dkwilson1991@gmail.com>
2022-11-21 17:29:41 +0900

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Destroying all users..."
+User.destroy_all
+puts "Destroying all Camps..."
+Camp.destroy_all
+puts "Destroying all Assignments..."
+Assignmet.destroy_all
+
+location = ["Japan, Tokyo", "Chile, Santiago", "Costa, Rica", "Havana, Cuba", "Iraq Bagdad", "Canada Ottawa", "Ukraine, Kyiv", "Spain, Madrid", "Philippines, Manila", "Tunisia, Tunis"]
+
+10.times do User.create!(
+  first_name: Faker::Name.unique.name.first,
+  last_name: Faker::Name.unique.name.last,
+  email: Faker::Internet.email
+)
+end
