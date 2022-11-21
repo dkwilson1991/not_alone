ba4983254cd19f359239f1c87e09bf3c603de975
Savithri Wewala <s.w.wewala@gmail.com>
2022-11-21 17:09:07 +0900

    added models and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,16 @@
 class User < ApplicationRecord
+  has_many :assignments
+  has_many :camps
+  has_one_attached :photo
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :location, presence: true
+  validates :birthday, presence: true
+  validates :training, presence: true
+  validates :admin_status, presence: true
 end
