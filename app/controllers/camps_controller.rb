c237ba13e7ddb3d3d9a3e5bb3f29020117f5ba13
Ayhem chelly <aihamchelly@gmail.com>
2022-11-22 14:27:20 +0900

    added fooTer,navbar etc.

--- app/controllers/camps_controller.rb
+++ app/controllers/camps_controller.rb
@@ -1,16 +1,41 @@
 class CampsController < ApplicationController
-  def show
-  end
-
   def index
+    @camps = Camp.all
   end
 
-  def edit
+  def show
+    @camp = Camp.find(params[:id])
+  end
+
+  def new
+    @camp = Camp.new
   end
 
   def create
+    @camp = Camp.new(params[:camp])
+    if @camp.save
+      redirect_to @camp, :notice => "Successfully created camp."
+    else
+      render :action => 'new'
+    end
+  end
+
+  def edit
+    @camp = Camp.find(params[:id])
   end
 
   def update
+    @camp = Camp.find(params[:id])
+    if @camp.update_attributes(params[:camp])
+      redirect_to @camp, :notice  => "Successfully updated camp."
+    else
+      render :action => 'edit'
+    end
+  end
+
+  def destroy
+    @camp = Camp.find(params[:id])
+    @camp.destroy
+    redirect_to camps_url, :notice => "Successfully destroyed camp."
   end
 end
