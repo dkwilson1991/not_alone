0d99b13c3c1de95aeb73505e69956ff16e986614
Savithri Wewala <s.w.wewala@gmail.com>
2022-11-23 12:27:01 +0900

    added @camps to home

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   include Pundit::Authorization
 
-  after_action :verify_authorized, except: :index, unless: :skip_pundit?
-  after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+  after_action :verify_authorized, except: %i[index home], unless: :skip_pundit?
+  after_action :verify_policy_scoped, only: %i[index home], unless: :skip_pundit?
 
   # Uncomment when you *really understand* Pundit!
   # rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
