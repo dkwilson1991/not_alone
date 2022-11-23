45b33688413ff3b0e802ccee3593fd4eb747a953
Savithri Wewala <s.w.wewala@gmail.com>
2022-11-23 18:00:47 +0900

    added newsfeed posts and cleaned up upcoming camps

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,7 @@ class PagesController < ApplicationController
 
   def home
     @camps = policy_scope(Camp)
+    @future_camps = Camp.where('start_date > ?', Date.today).order(:start_date).first(3)
+    @past_camps = Camp.where('start_date <= ?', Date.today).order(:start_date)
   end
 end
