81fa563abb1a55e514f4e4854a8c5fdf52246140
Yulia Naumenko <naumenkoyuliya@ukr.net>
2022-11-23 11:26:29 +0900

    Working calendar

--- Gemfile
+++ Gemfile
@@ -56,6 +56,7 @@ gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'main
 gem "font-awesome-sass", "~> 6.1"
 gem "pundit"
 gem "simple_form", github: "heartcombo/simple_form"
+gem "simple_calendar", "~> 2.4"
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -2,9 +2,14 @@ class AssignmentsController < ApplicationController
   def index
     @assignments = policy_scope(Assignment)
     @camp = Camp.new
+    # start_date = params.fetch(:start_date, Date.today).to_date
+    # @assignments = Assignment.where(starts_at: start_date.beginning_of_week..start_date.end_of_week)
   end
 
   def show
+    @assignment = Assignment.find(params[:id])
+    @camp = Camp.new
+    authorize @camp
   end
 
   def create
