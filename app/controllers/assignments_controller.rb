6378fc71b4bb604fd22f21d9b9d2eb3bc0a42528
Yulia Naumenko <naumenkoyuliya@ukr.net>
2022-11-24 18:26:06 +0900

    saving

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -2,6 +2,9 @@ class AssignmentsController < ApplicationController
   def index
     @assignments = policy_scope(Assignment)
     @camp = Camp.new
+    @future_camps = Camp.where('start_date > ?', Date.today).order(:start_date).first(3)
+    @past_camps = Camp.where('start_date <= ?', Date.today).order(:start_date)
+
     # start_date = params.fetch(:start_date, Date.today).to_date
     # @assignments = Assignment.where(starts_at: start_date.beginning_of_week..start_date.end_of_week)
   end
