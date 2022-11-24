e2b88054446e8a8452ad5d8e6a628e5e4f50d62c
Yulia Naumenko <naumenkoyuliya@ukr.net>
2022-11-24 14:36:30 +0900

    working on it

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -29,11 +29,11 @@ export default class extends Controller {
   //   })
   // }
 
-  // #fitMapToMarkers() {
-  //   const bounds = new mapboxgl.LngLatBounds()
-  //   this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
-  //   this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 1000 })
-  // }
+  #fitMapToMarkers() {
+    const bounds = new mapboxgl.LngLatBounds()
+    this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 1000 })
+  }
 
   #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
