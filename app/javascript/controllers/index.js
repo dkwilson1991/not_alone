887090264414fbfd7506177eb077ceb3a225fa55
Yulia Naumenko <naumenkoyuliya@ukr.net>
2022-11-25 11:04:33 +0900

    fixed seeds

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,6 +4,9 @@
 
 import { application } from "./application"
 
+// import AddressAutocompleteController from "./address_autocomplete_controller"
+// application.register("address-autocomplete", AddressAutocompleteController)
+
 import CreateAssignmentController from "./create_assignment_controller"
 application.register("create-assignment", CreateAssignmentController)
 
