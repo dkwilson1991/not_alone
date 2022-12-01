c8e7ff070d566be1b4e3a37f4fa46f733fa13c94
Ayhem chelly <aihamchelly@gmail.com>
2022-12-01 17:42:30 +0900

    donate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_30_014958) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_01_052449) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -137,6 +137,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_30_014958) do
     t.boolean "admin_status", default: false
     t.float "latitude"
     t.float "longitude"
+    t.string "applied_to"
+    t.integer "user_number"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
