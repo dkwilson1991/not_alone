3369072d19dc31f296d5a4982558fd711fa7e887
Savithri Wewala <s.w.wewala@gmail.com>
2022-12-02 15:37:20 +0900

    changed mailer

--- app/jobs/mailer_job.rb
+++ app/jobs/mailer_job.rb
@@ -3,11 +3,13 @@ class MailerJob < ApplicationJob
 
   def perform(camp)
     @users = User.all.where('location = ?', 'Sumy, Ukraine')
-    @users.each do |user|
+    roles = [["Crew Leader", "Crew Leader"], ["Story Telling", "Station Leader: Story Telling"], ["Snacks", "Station Leader: Snacks"], ["Crafts", "Station Leader: Crafts"], ["Games", "Station Leader: Games"]]
+    @users.each_with_index do |user, index|
       @assignment = Assignment.new
       @assignment.camp = camp
       @assignment.user = user
-      @assignment.status = 'pending'
+      @assignment.role = roles[index]
+      @assignment.status = 'admin_confirm'
       @assignment.save
     end
   end
